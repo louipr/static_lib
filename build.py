fcfa381887e2fb7f0a605870ff73131399fda327
Luis Pabon <louipr@gmail.com>
2019-03-10 19:11:20 -0700

    working linker

--- build.py
+++ build.py
@@ -3,7 +3,7 @@ import os
 
 def clean():
     files = [
-        "*.exe",
+        "*.a",
         "*.obj"
     ]
     for fname in files:
@@ -15,7 +15,8 @@ def clean():
 def compile(compile_list):
     for src_file in compile_list:
         obj_name = src_file.split(".")[0] + ".obj"
-        cmd = "gcc -c -g %s -o %s"%(src_file,obj_name)
+        cmd = "clang -c -x c %s -o %s"%(src_file,obj_name)
+        
         print(cmd)
         if(os.system(cmd)):
             print("Aborting compile process!")
@@ -30,7 +31,7 @@ def staticlib(obj_file):
 
 def link(obj_list):
     link_str = " ".join(obj_list)
-    cmd = "ld -o out.exe " + link_str
+    cmd = "/Library/Developer/CommandLineTools/usr/bin/ld " + link_str
     print(cmd)
     if(os.system(cmd)):
         print("Aborting link process")
@@ -48,29 +49,15 @@ def main():
     # staticlib("main.obj")
 
     obj_list = [src_file.split(".c")[0] + ".obj" for src_file in compile_list]
-    # obj_list = [obj_file for obj_file in obj_list if obj_file != "main.obj"]
-    obj_list.append("-L/usr/lib/gcc/x86_64-pc-cygwin/7.3.0")
-    #obj_list.append("-L/usr/x86_64-pc-cygwin/lib")
-    #obj_list.append("-L/usr/lib")
-    obj_list.append("-L/lib")
-    obj_list.append("-lc")
-    obj_list.append("-lgcc_s")
-    obj_list.append("-lgcc")
-    obj_list.append("-lcygwin")
-    obj_list.append("-ladvapi32")
-    obj_list.append("-lshell32")
-    obj_list.append("-luser32")
-    obj_list.append("-lkernel32")
-    # obj_list.append("/usr/lib/gcc/x86_64-pc-cygwin/7.3.0/../../../../lib/default-manifest.o")
-    # obj_list.append("/usr/lib/gcc/x86_64-pc-cygwin/7.3.0/../../../../lib/crt0.o")
-    # obj_list.append("/usr/lib/gcc/x86_64-pc-cygwin/7.3.0/crtbegin.o")
-    # obj_list.append("/usr/lib/gcc/x86_64-pc-cygwin/7.3.0/crtend.o")
-    #obj_list.append("/usr/lib/gcc/x86_64-pc-cygwin/7.3.0/cyglto_plugin.dll") 
-    #WinMainCRTStartup
-    #obj_list.append("/usr/lib/gcc/x86_64-pc-cygwin/7.3.0/collect2.exe") 
-    # obj_list.append("/usr/lib/gcc/x86_64-pc-cygwin/7.3.0/lto-wrapper.exe") 
-    # obj_list.append("--entry WinMainCRTStartup")
-    # obj_list.append("-nodefaultlib")
+    #obj_list.append("-dynamic -dylib -arch x86_64 -macosx_version_min 10.12.0")
+    obj_list.append("-demangle")
+    obj_list.append("-lto_library /Library/Developer/CommandLineTools/usr/lib/libLTO.dylib")
+    obj_list.append("-no_deduplicate -dynamic -arch x86_64 -macosx_version_min 10.12.0")
+    obj_list.append("-o out.a")
+    obj_list.append("-lSystem /Library/Developer/CommandLineTools/usr/lib/clang/9.0.0/lib/darwin/libclang_rt.osx.a")
+    obj_list.append("")
+    obj_list.append("")
+    obj_list.append("")
     link(obj_list)
 
 if __name__ == "__main__":
